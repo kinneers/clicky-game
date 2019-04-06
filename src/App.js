ac3e30413a0fba10de2ee293682c34feac1c71ba
Sarah Kinneer <kinneers@gmail.com>
2019-04-06 15:41:58 -0400

    Current score and high score logic working

--- src/App.js
+++ src/App.js
@@ -3,32 +3,37 @@ import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import NavScore from "./components/NavScore";
 import ImageCard from "./components/ImageCard";
+//import Shuffle from 'shufflejs';
 import images from "./images.json";
 
 class App extends Component {
 
     state = {
         score: 0,
-        images
+        images,
+        highScore: 0
     };
 
     componentDidMount(shuffled) {
         console.log("Component Mounted");
-        // this.shuffleImages(images);
-        // this.setState({ images: images });
+        
     }
 
-    shuffleImages = (images) => {
-        for (let i of images) {
-            const j = Math.floor(Math.random() * (i+1));
-            [images[i], images[j]] = [images[j], images[i]];
-        };
-    }
+    // shuffleImages = (images) => {
+    //     for (let i of images) {
+    //         const j = Math.floor(Math.random() * (i+1));
+    //         [images[i], images[j]] = [images[j], images[i]];
+    //     };
+    // }
 
     //Called within the handleClick function when an image is newly clicked
     updateScore = () => {
         //Takes the previous score from state and updates it by one
-        this.setState(prevState => { return { score: prevState.score + 1 }});
+        if (this.state.score >= this.state.highScore) {
+            this.setState(prevState => { return { score: prevState.score + 1, highScore: this.state.score + 1 }});
+        } else {
+            this.setState(prevState => { return { score: prevState.score + 1 }});
+        };
     }
     
     resetGame = () => {
@@ -39,12 +44,12 @@ class App extends Component {
     render() {
         return (
             <div>
-                <NavScore></NavScore>
+                <NavScore score={this.state.score} highScore={this.state.highScore}></NavScore>
                 <Title>This is the Title</Title>
                 <Wrapper>
-                    {this.state.images.map(image => (
+                    {this.state.images.map((image, i) => (
                         <ImageCard
-                            key={image.id}
+                            key={i}
                             id={image.id}
                             image={image.source}
                             name={image.name}
--- src/components/NavScore/index.js
+++ src/components/NavScore/index.js
@@ -5,14 +5,15 @@ function NavScore(props) {
     return (
         <ul className="nav nav-pills nav-fill">
             <li className="nav-item">
-                <a className="nav-link active" href="http://google.com">Change this to refresh the page</a>
+                <a className="nav-link active" href="./index.html"><h2>Clicky Game!</h2></a>
             </li>
             <li className="nav-item">
             </li>
             <li className="nav-item">
+                <h2 className="score">Current Score: {props.score}/12</h2>
             </li>
             <li className="nav-item">
-                <h2 className="score">Score</h2>
+                <h2 className="score">High Score: {props.highScore}/12</h2>
             </li>
         </ul>
     );
