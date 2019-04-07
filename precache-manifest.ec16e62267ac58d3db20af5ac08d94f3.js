c3752cee8400ba3c527b98d28d10c22f0ecafbcf
Sarah Kinneer <kinneers@gmail.com>
2019-04-06 21:42:27 -0400

    Updates

---
+++ precache-manifest.ec16e62267ac58d3db20af5ac08d94f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "8950788c191d77d2ed5d",
+    "url": "/clicky-game/static/js/main.5135fa56.chunk.js"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/clicky-game/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "8950788c191d77d2ed5d",
+    "url": "/clicky-game/static/css/main.c53dd4a9.chunk.css"
+  },
+  {
+    "revision": "36930606333f234a9dcf6b602672a660",
+    "url": "/clicky-game/index.html"
+  }
+];
